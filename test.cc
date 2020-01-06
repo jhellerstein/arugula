2b26b95be3457c6c03d101317f360d69ade69c85
Joe Hellerstein <jmh@berkeley.edu>
2020-01-06 01:30:47 -0800

    unordered_set test

--- test.cc
+++ test.cc
@@ -167,6 +167,98 @@ TEST_CASE("SetUnion Wrap") {
   }  
 }
 
+TEST_CASE("UnorderedSetUnion") {
+  std::unordered_set<int> lefts({1, 20, 30});
+  std::unordered_set<int> rights({1, 2, 3});
+
+  Lattice ls(lefts, Union{});
+  Lattice rs(rights, Union{});
+
+  auto expr = ls + rs;
+
+  REQUIRE(expr.reveal().size() == 5);
+  for (auto &i : lefts) {
+    REQUIRE(expr.reveal().find(i) != expr.reveal().end());
+  }
+  for (auto &i : lefts) {
+    REQUIRE(expr.reveal().find(i) != expr.reveal().end());
+  }
+}
+
+TEST_CASE("UnorderedSetUnion merges") {
+  std::unordered_set<int> lefts({1, 20, 30});
+  std::unordered_set<int> rights({1, 2, 3});
+  std::unordered_set<int> others({1, 200, 300});
+
+  Lattice ls(std::move(lefts), Union{});
+  Lattice rs(std::move(rights), Union{});
+
+  ls += rs;
+  REQUIRE(ls.reveal().size() == 5);
+  for (auto i : {1,2,3,20,30}) {
+    REQUIRE(ls.reveal().find(i) != ls.reveal().end());
+  }
+
+  ls += others;
+  REQUIRE(ls.reveal().size() == 7);
+  for (auto i : {1,2,3,20,30,200,300}) {
+    REQUIRE(ls.reveal().find(i) != ls.reveal().end());
+  }
+}
+
+TEST_CASE("UnorderedSetUnion Equality") {
+  std::unordered_set<int> first_order({1, 20, 30});
+  std::unordered_set<int> first_unorder({20, 1, 30});
+  std::unordered_set<int> evens({2, 4, 6});
+  std::unordered_set<int> second_order({1, 20, 30});
+
+  Lattice lfo(first_order, Union{});
+  Lattice lfu(first_unorder, Union{});
+  Lattice le(evens, Union{});
+  Lattice lso(second_order, Union{});
+  
+  REQUIRE(lfo != lfu);
+  REQUIRE(lfo != le);
+  REQUIRE(lfo == lso);
+}
+
+TEST_CASE("UnorderedSetUnion Assign") {
+  std::unordered_set<int> negnums({-1,-2,-3});
+  std::unordered_set<int> nums({1,2,3});
+  std::unordered_set<int> mt;
+
+  Lattice ls(std::move(mt), Union{});
+  ls = negnums;
+  REQUIRE(ls.reveal().size() == 3);
+  for (auto i : negnums) {
+    REQUIRE(ls.reveal().find(i) != ls.reveal().end());
+  }  
+  REQUIRE(negnums.size() == 3);
+  Lattice ls2(std::move(nums), Union{});
+  ls = ls2;
+  REQUIRE(ls.reveal().size() == 3);
+  for (auto i : {1,2,3}) {
+    REQUIRE(ls.reveal().find(i) != ls.reveal().end());
+  }  
+}
+
+TEST_CASE("UnorderedSetUnion Wrap") {
+  std::unordered_set<int> negnums({-1,-2,-3});
+  std::unordered_set<int> nums({1,2,3});
+  std::unordered_set<int> mt;
+
+  Lattice ls(std::move(mt), Union{});
+  ls.wrap(std::move(negnums));
+  REQUIRE(ls.reveal().size() == 3);
+  REQUIRE(negnums.size() == 0);
+  ls.wrap(std::move(nums));
+  REQUIRE(ls.reveal().size() == 3);
+  REQUIRE(nums.size() == 0);
+  for (auto i : {1,2,3}) {
+    REQUIRE(ls.reveal().find(i) != ls.reveal().end());
+  }  
+}
+
 TEST_CASE("SetUnionUnique") {
   // This fails to compile with std::set<std::unique_ptr<int> >
   std::set<std::shared_ptr<int>, PointerCmp> lefts;
